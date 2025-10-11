16eb99f751863ed95b39e9564574d5791517beb5
Queenlicia0502 <twinskia@gmail.com>
2025-10-11 12:30:26 +0300

    initial commit

---
+++ identity_operater.py
@@ -0,0 +1,26 @@
+#Python program to illustrate the use
+# of 'is' identity operator
+x = 5
+if (type(x) is int):
+    print("true")
+else:
+    print("flase")
+
+
+
+
+
+x = 5.0
+if (type(x) is not float):
+    print("true")
+else:
+    print("flase")
+
+
+x = 20
+y = 20
+if ( x is y ):
+    print("x & y SAME identity")
+y=30
+if ( x is not y ):
+    print("x & y have DIFFERENT identity")

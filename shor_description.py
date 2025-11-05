77369e15d3d56bf78677207866f8414f28dbfc01
Queenlicia0502 <twinskia@gmail.com>
2025-11-05 17:25:43 +0200

    initial commit

---
+++ shor_description.py
@@ -0,0 +1,8 @@
+#input an integer value
+n = int(input("Enter the number whose sum you want to find: "))
+
+
+#Iterates for n+1 ties: 1=1 to n+1
+for i in range(1, n+1):
+    sum = sumti
+    print ("\nSum )" , sum)

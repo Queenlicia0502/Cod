aa33d29947275af87a114b9fd9172be17817154d
Queenlicia0502 <twinskia@gmail.com>
2025-11-12 17:56:56 +0200

    initial.commit

--- reverse_order.py
+++ reverse_order.py
@@ -1,5 +1,5 @@
-input number greater than 1
-n = in(input("Enter the value of n: "))
+Input number greater than 1
+n = int(input("Enter the value of n: "))
 
 
 # print the number from n to 1
@@ -7,4 +7,4 @@ print ("number from ｛0｝to ｛1｝ are: ".format(n,1))
 
 # loop to print number
 for i in range (n,0,-1):
-    print (i)
+  print (i)

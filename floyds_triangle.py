5e676721450829eaf87d736dd50a5e22a844d634
Queenlicia0502 <twinskia@gmail.com>
2025-11-26 17:39:19 +0200

    initials commit

---
+++ floyds_triangle.py
@@ -0,0 +1,13 @@
+#Take input from user
+rows = int(input("Please Enter the total Number of Rows : "))
+number = 1 #initialise by 1
+
+print("Floyed's Triangle")
+#outer loop for number of rows
+# for j in range(1,  i + 1):
+  #inner loop for numble of columns
+  # for j in range(1, i  + 1):
+    #display result 
+    # print(number, end = '  ')
+    # number = number + 1
+print()

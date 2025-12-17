eb67ee688d70c83dd90b65f3926656a090230bbb
Queenlicia0502 <twinskia@gmail.com>
2025-12-17 17:57:48 +0200

    initial comitial

---
+++ Tip_the_waiter.py
@@ -0,0 +1,12 @@
+def total_calc(bill_amountt,tip_perc):
+#define function to calculate the tip on bill
+  total = bill_amount*(1 + 0.01*tip_perc)
+  total = round(total,2)
+  print(f"Plese pay ${total}")
+
+
+  # specify only bill_amout
+  # defult value of tip percentage is used
+
+  total_calc(150,20)
+    

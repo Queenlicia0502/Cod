3aca20c4ad869a9284b7c48e5f8a9220615650c4
Queenlicia0502 <twinskia@gmail.com>
2026-01-07 17:34:09 +0200

    initial commit

---
+++ Break.py
@@ -0,0 +1,11 @@
+#Take user input
+a = input ("Enter a word: ")
+#program to check break keyword
+for i in a: #iterate for loop
+   if (i == 'A'): #condition 1
+    #display result
+    print ("B is found")
+    break #break staatment
+   else:
+     #display Result
+     print ("B not found")

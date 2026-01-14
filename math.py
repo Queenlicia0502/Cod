836a2e7826f38f95f0d6e993f37f0ea3ce5c2052
Queenlicia0502 <twinskia@gmail.com>
2026-01-14 17:58:05 +0200

    initial.commit

---
+++ math.py
@@ -0,0 +1,10 @@
+import math #importing math module
+#using ceil and floor function of math moddule
+print('The Floor and Celling value of 23.56 are: ' + str(math.ceil(23.56)) + ', ' + str (math.floor(23.56)))
+
+x = 10
+y = -15
+#using fabs and gcd function
+print('Absolute value of -96 and 56 are: ' + str(math.fabs(-96)))
+   
+print('The GCD of 24 and 56 : ' + str(math.gcd(24, 56)))
